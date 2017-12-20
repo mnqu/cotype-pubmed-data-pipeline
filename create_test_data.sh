cf7d0a101039633fc7615ec8480204e187021ed4
mnqu <qumn123@gmail.com>
2017-12-20 14:25:29 -0600

    modified script files

--- create_test_data.sh
+++ create_test_data.sh
@@ -4,9 +4,14 @@
 umls_path=/shared/data/mengqu2/umls/cotype/
 
 # NER path
-ner_model=${data_path}stanford-ner/ner_calbc.sh
+ner_model=${umls_path}stanford-ner/ner_calbc.sh
 
 # input file
+# name2cui_umls: Mapping file from UMLS entity name to entity id. Each line of the file contains a name and an id, separated by '\t'.
+# name2cui_go: Mapping file from GO entity name to entity id. Each line of the file contains a name and an id, separated by '\t'.
+# cui2type_umls: Mapping file from UMLS entity id to entity type. Each line of the file contains an id and a type, separated by '\t'.
+# cui2type_go: Mapping file from GO entity id to entity type. Each line of the file contains an id and a type, separated by '\t'.
+# pair2relation: Mapping file from entity pair to relation. Each line of the file contains two id and a relation, separated by '\t'.
 name2cui_umls=${umls_path}name2cui_umls.txt
 name2cui_go=${umls_path}name2cui_go.txt
 cui2type_umls=${umls_path}cui2type_umls.txt
@@ -16,7 +21,7 @@ pair2relation=${umls_path}pair2relation.txt
 text_file=text.txt
 
 # output file
-output_json_file=data_pubmed.json
+output_json_file=data_test.json
 
 ${ner_model} ${text_file} text_ner.txt
 python process.py text_ner.txt ${name2cui_umls} ${name2cui_go} ${cui2type_umls} ${cui2type_go} ${pair2relation} ${output_json_file}
--- create_training_data.sh
+++ create_training_data.sh
@@ -4,6 +4,11 @@
 umls_path=/shared/data/mengqu2/umls/cotype/
 
 # input file
+# name2cui_umls: Mapping file from UMLS entity name to entity id. Each line of the file contains a name and an id, separated by '\t'.
+# name2cui_go: Mapping file from GO entity name to entity id. Each line of the file contains a name and an id, separated by '\t'.
+# cui2type_umls: Mapping file from UMLS entity id to entity type. Each line of the file contains an id and a type, separated by '\t'.
+# cui2type_go: Mapping file from GO entity id to entity type. Each line of the file contains an id and a type, separated by '\t'.
+# pair2relation: Mapping file from entity pair to relation. Each line of the file contains two id and a relation, separated by '\t'.
 name2cui_umls=${umls_path}name2cui_umls.txt
 name2cui_go=${umls_path}name2cui_go.txt
 cui2type_umls=${umls_path}cui2type_umls.txt
@@ -12,7 +17,7 @@ pair2relation=${umls_path}pair2relation.txt
 text_file=${umls_path}pubmed.token
 
 # output file
-output_json_file=data_pubmed.json
+output_json_file=data_train.json
 output_instance_file=instance_pubmed.txt
 
 python distant_supervision.py ${name2cui_umls} ${name2cui_go} ${pair2relation} ${text_file} temp.json stopwords.txt ${output_instance_file}
